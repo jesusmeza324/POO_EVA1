27caf7d24787ce5cf88beba79a2ab73f12056319
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2025-02-05 21:40:57 -0600

    Add files via upload

---
+++ EVA1_2_MODIFICADOR/src/main/java/Paquete2/ClassB.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Paquete2;
+
+/**
+ *
+ * @author jesus
+ */
+public class ClassB {
+    public int valorPublic;
+    private int valorPrivado;
+    int valorDefault;
+    
+}
+class ClassTwo{//default
+    
+}
---
+++ EVA1_2_MODIFICADOR/src/main/java/com/mycompany/eva1_2_modificador/EVA1_2_MODIFICADOR.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_2_modificador;
+
+import Paquete2.ClassB;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA1_2_MODIFICADOR {
+
+    public static void main(String[] args) {
+       //crear objetos:instanciar:
+       //clase identificador =new clase:
+       ClassB objB =new ClassB();
+       objB.valorPublic=100;//nivel de accesso publico
+       //los otros no son visibles aqui
+       //ClassTwo objTwo=new ClassTwo();//clase deafault no se puede ver aqui
+        
+    }
+}
