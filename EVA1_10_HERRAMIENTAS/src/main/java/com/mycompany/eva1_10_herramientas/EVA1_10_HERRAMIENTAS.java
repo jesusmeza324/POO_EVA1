cab5ec2ff1f0c23a47cfc0198cf0b2e7976f3222
jesusmeza324 <L24550235@chihuahua2.tecnm.mx>
2025-02-17 21:14:23 -0600

    Add files via upload

---
+++ EVA1_10_HERRAMIENTAS/src/main/java/com/mycompany/eva1_10_herramientas/EVA1_10_HERRAMIENTAS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva1_10_herramientas;
+
+/**
+ *
+ * @author jesus
+ */
+public class EVA1_10_HERRAMIENTAS {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ EVA1_10_HERRAMIENTAS/src/main/java/com/mycompany/eva1_10_herramientas/Personas.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.eva1_10_herramientas;
+
+/**
+ *
+ * @author jesus
+ */
+public class Personas {
+    private String nombre;
+    private int edad;
+    private char genero;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public char getGenero() {
+        return genero;
+    }
+
+    public void setGenero(char genero) {
+        this.genero = genero;
+    }
+    
+}
